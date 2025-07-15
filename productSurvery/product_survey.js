9e4bfe930c8df4ea9e00ed52f26c4e1e7a25cafd
Ian Kirk <iankirk24@gmail.com>
2025-07-15 18:20:22 -0400

    added Practice task to productSurvery folder

--- productSurvery/product_survey.js
+++ productSurvery/product_survey.js
@@ -7,6 +7,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('userExperience').value;
 
 
     document.getElementById('userName').innerHTML = username;
@@ -16,6 +17,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userEx').innerHTML = userExperience;
 
     document.getElementById('userInfo').style.display = 'block';
     alert('Thank you for your valuable feedback');    
