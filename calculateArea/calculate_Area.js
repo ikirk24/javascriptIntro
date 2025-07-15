e967d964aa6cb98ff0fa6068348823562580ea98
Ian Kirk <iankirk24@gmail.com>
2025-07-15 17:26:22 -0400

    Added groceryTracker to calculateArea folder

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,13 @@ function calculateArea() {
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function groceryTracker () {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let total = grocery1 + grocery2 + grocery3;
+
+    document.getElementById('total').innerText = `You spent $${total} at the grocery store today`;
+}
