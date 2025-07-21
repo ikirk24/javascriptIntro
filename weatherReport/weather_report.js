1b70e2a32b1a1fc9c0b89b4a02dcbb2f0d1211e3
Ian Kirk <iankirk24@gmail.com>
2025-07-21 13:57:14 -0400

    Added practice taske to weatherReport

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -21,3 +21,27 @@ function showweatherDetails(event) {
 }
 
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+
+function showweatherTwo(event) {
+    event.preventDefault();
+    const lat = document.getElementById('lat').value;
+    const lon = document.getElementById('lon').value;
+    const apiKey = '5cd035dca1ecf9d401948618b097b434';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2> Weather in ${data.name}</h2>
+                                     <p>Temperature: ${data.main.temp} &#8451;</p>
+                                     <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+            console.error('Error fetching weather:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+          });
+}
+
+document.getElementById('weatherForm2').addEventListener('submit', showweatherTwo );
